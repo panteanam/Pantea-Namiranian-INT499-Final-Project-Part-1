7e144db7fadd9c34f0e7683f679943f7ba5418f3
panteanam <Panteanam@gmail.com>
2025-06-16 18:57:11 -0700

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+    "short_name": "StreamList",
+    "name": "StreamList App",
+    "icons": [
+        {
+            "src": "favicon.ico",
+            "sizes": "any",
+            "type": "image/x-icon"
+        }
+    ],
+    "start_url": ".",
+    "display": "standalone",
+    "theme_color": "#ffffff",
+    "background_color": "#ffffff"
+}
